ca63a3dc4ee3a715b5aeec7e1b91e2ef263bcd01
Ayanami <Ayanami@bupt.edu.cn>
2023-03-13 23:35:49 +0800

    first

--- PasswordGenerator/gen_01.py
+++ PasswordGenerator/gen_01.py
@@ -4,28 +4,41 @@ password = input('请输入新密码：')
 UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 LOWER = 'abcdefghijklmnopqrstuvwxyz'
 DIGIT = '0123456789'
-PUNC = '`,.<>?:'"\\{}[]()*&^%$#@!/+=-_ |"
 
 have_upper = False
 have_lower = False
 have_digit = False
 have_punc = False
-is_secure = False
 
 for char in password:
     if char in UPPER:
         have_upper = True
-    if char in LOWER:
+    elif char in LOWER:
         have_lower = True
-    if char in DIGIT:
+    elif char in DIGIT:
         have_digit = True
-    if char in PUNC:
+    else:
         have_punc = True
-    if have_upper and have_lower and have_digit and have_punc:
-        is_secure = True
-        print('密码安全性合格\n')
-        break
-if not is_secure:
-    print('密码不安全，请重新设置\n')
 
+    have_enough = len(password) == 8
+    is_secure = (have_enough and have_upper
+             and have_lower and have_digit
+             and have_punc)
 # TODO：输出
+    if is_secure:
+        print('密码安全性合格。\n')
+        break
+    else:
+        prompt = '密码不符合，'
+        if not have_enough:
+            prompt += '长度不符，'
+        if not have_upper:
+            prompt += '不包含大写字符，'
+        if not have_lower:
+            prompt += '不包含小写字符，'
+        if not have_digit:
+            prompt += '不包含数字，'
+        if not have_punc:
+            prompt += '不包含特殊符号，'
+prompt = prompt[:-1]
+print(prompt)
